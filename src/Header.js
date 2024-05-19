49552efecb3f7f4b757b7e5921e800c0f686e2aa
zeldyf <123342002+zeldyf@users.noreply.github.com>
2024-05-19 15:18:26 -0400

    reformat

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import './Header.css'
+import React from "react";
+import "./Header.css";
 function Header() {
   return (
-    <div className='header'>
-              <img className='header__logo'
-               src='https://i.imgur.com/ZxEabZ0.png' 
-               alt='J-Sit'/>
-             
-      
+    <div className="header">
+      <img
+        className="header__logo"
+        src="https://i.imgur.com/ZxEabZ0.png"
+        alt="J-Sit"
+      />
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
