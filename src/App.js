ad1eae755785af7b0b6f5411dd7cc1dca95057ec
Rivka Weingarten <rw240brooklyn@gmail.com>
2024-05-19 16:51:23 -0400

    babysitter form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import MomSignUp from "./screens/MomSignUp.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      {/* <CurrentUserProvider> */}
+        <BrowserRouter>
+          {/* <Header /> */}
+          <Routes>
+            {/* <Route path="/" element={<HomeScreen />} /> */}
+            {/* <Route path="/sitter-login" element={<SitterLoginScreen />}></Route>
+            <Route path="/mom-login" element={<MomLoginScreen />}></Route> */}
+            {/* <Route path="/sitter-signup" element={<SitterSignUp />}></Route> */}
+            <Route path="/mom-signup" element={<MomSignUp />}></Route>
+                     </Routes>
+        </BrowserRouter>
+      {/* </CurrentUserProvider> */}
     </div>
   );
 }
