42acdbda266ad6c32f9bbdd2d1840697f9ce696b
Jessica Kiroff <jesskiroff@gmail.com>
2024-05-19 11:45:27 -0400

    initialize homepage

---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <div className='home'>
+        <div className='home__container'>
+        <img
+          className="home__image"
+          src=""
+          alt=""
+        />  
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
