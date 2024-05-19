803b0b90ce416f8ac10ff33a0adbc61673f18645
zeldyf <123342002+zeldyf@users.noreply.github.com>
2024-05-19 17:32:05 -0400

    add search form to UI

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import BabysitterCards from './BabysitterCards'
+import React from "react";
+import BabysitterCards from "./BabysitterCards";
+import SearchForm from "./SearchForm";
 
 function Homepage() {
   return (
     <>
-    <div className="container text-center">
+      <div className="container text-center">
+        <SearchForm />
         <div className="row justify-content-center">
           {/* render all of them */}
           <BabysitterCards />
         </div>
-    </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Homepage
+export default Homepage;
