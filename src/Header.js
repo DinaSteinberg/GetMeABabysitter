e37a15198012b1b2ccdf714cfa5f8348c6e19a61
Jessica Kiroff <jesskiroff@gmail.com>
2024-05-19 18:50:53 -0400

    initialize styling

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,12 @@
-import React from "react";
-
+import React from 'react';
+import './Header.css';
 function Header() {
   return (
     <div className="header">
       <h1>JSit</h1>
-      <p>The Jewish Babysitter Shadchan</p>
+      <div className="title">
+        <p>The Jewish Babysitter Shadchan</p>
+      </div>
     </div>
   );
 }
